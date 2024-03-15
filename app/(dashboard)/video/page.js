1d65f4d5bce9474bdde72584bc33a044b5a770c6
Ajaymaurya1008 <ajaykvmaurya@gmail.com>
2024-03-16 00:01:30 +0530

    Refactor video call page to improve performance

--- app/(dashboard)/video/page.js
+++ app/(dashboard)/video/page.js
@@ -1,7 +1,6 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
-import AgoraUIKit from "agora-react-uikit";
-import dynamic from "next/dynamic";
 
 const VideoCallPage = () => {
   const [videoCall, setVideoCall] = useState(false);
@@ -10,9 +9,9 @@ const VideoCallPage = () => {
     EndCall: () => setVideoCall(false),
   };
 
-  const AgoraUIKit = dynamic(() => import("agora-react-uikit"), {
-    ssr: false,
-  });
+
+  const AgoraUIKit =
+    typeof window !== "undefined" && require("agora-react-uikit");
 
   const rtcProps = {
     appId: "ced4e343c11f4cefb701f5c8ec2bf880",
@@ -20,9 +19,10 @@ const VideoCallPage = () => {
     token:
       "007eJxTYMgNW7T7zCar0uTDix99Ot2i67HHIfHeljXhdb9mTDRhrN2owJCUapqWbJGanGqcZmliaG5qYWGUlGxoZphmkJJkaJiS7BnyMbUhkJHhVXw6CyMDBIL4PAyOBQX5mXklual5JYYMDABGUiVR",
   };
+
   return videoCall ? (
     <div style={{ display: "flex", height: "100vh" }}>
-      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+      {AgoraUIKit && <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />}
     </div>
   ) : (
     <button
